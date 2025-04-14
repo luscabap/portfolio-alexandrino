e31e34ef4910702e2ca9fe0271bcf546ab22b69b
luscabap <lucasbaptistasilva.dev@gmail.com>
2025-04-14 20:12:23 -0300

    feat: removing styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Apresentation from "./components/apresentation";
+import Header from "./components/header";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <Header />
       <Apresentation />
     </div>
   );
