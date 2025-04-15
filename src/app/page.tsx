f6a8fffd7e2732c07fd924bb400535a1a7f07e2f
luscabap <lucasbaptistasilva.dev@gmail.com>
2025-04-15 20:57:19 -0300

    feat: adjusting styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
-import Apresentation from "./components/apresentation";
+import About from "./components/about";
 import Header from "./components/header";
+import Skills from "./components/skills";
+import Footer from "./components/footer";
+import Contact from "./components/contact";
+import Projects from "./components/projects";
+import Apresentation from "./components/apresentation";
 
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
+    <div className="font-[family-name:var(--font-geist-sans)] flex flex-col items-center justify-start gap-12 text-center">
       <Header />
       <Apresentation />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+      <span>Ideia de ter algum depoimento do pessoal que já trabalhou contigo ou estudo te elogiando</span>
+      <Footer />
     </div>
   );
 }
