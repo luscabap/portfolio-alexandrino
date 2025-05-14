5a245f2b2ae7ce03283ea33a7f99d692e3643829
luscabap <lucasbaptistasilva.dev@gmail.com>
2025-05-14 18:34:35 -0300

    base text

--- src/app/components/about/index.tsx
+++ src/app/components/about/index.tsx
@@ -1,10 +1,14 @@
 export default function About(){
 
   return (
-    <div className="bg-zinc-400 p-4">
-      <span>Falar sobre sua história, trajetoria profissional, principais areas de atuação</span>
-      <p className="indent-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti, ducimus consectetur autem distinctio assumenda, fugit esse architecto nostrum provident laboriosam vel eveniet saepe ea excepturi dolores reiciendis exercitationem perferendis quo!</p>
-      <p className="indent-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum reprehenderit fugit ut perferendis quisquam illo ipsa quaerat corporis? Sint possimus rerum accusantium dolor qui fuga excepturi vel nisi accusamus veritatis?</p>
+    <div className="bg-zinc-400 p-4 flex flex-col gap-4 text-zinc-900">
+      <p className="indent-3">Alexandrino Alves dos Santos Neto, natural de Recife, Pernambuco, tenho 34 anos e sou formado em Processos Gerenciais, além de estar em constante atualização como pós-graduando em Gestão de Processos (Business Process Management).</p>
+      <p className="indent-3">Ao longo da carreira, destaco-me como gestor em empresas de segmentos distintos, acumulando uma sólida experiência na otimização e reestruturação de processos organizacionais.</p>
+      <p className="indent-3">Na VN Advocacia e Assessoria Jurídica, assumi o papel de gestor com o objetivo de eliminar gargalos, redesenhar fluxos de trabalho e implementar soluções estratégicas que resultaram em maior eficiência e redução de custos excessivos.</p>
+      <p className="indent-3">Essa atuação trouxe impactos significativos, alinhando a operação às melhores práticas de mercado e gerando resultados sustentáveis para a organização.</p>
+      <p className="indent-3">Posteriormente, na Rações Nutri Vida Ltda, apliquei a expertise em gestão de processos para solucionar desafios similares, ajustando as operações para promover eficiência, economia e melhoria contínua.</p>
+      <p className="indent-3">Meu foco em inovação, alinhado à busca incessante por eficiência e excelência operacional, reflete um profissional dinâmico e orientado a resultados, sempre comprometido com o sucesso das organizações em que atua.</p>
+      <p className="indent-3">Essa trajetória evidencia minha qualificação profissional e preparo para enfrentar os desafios da gestão moderna, com capacidade de transformar cenários complexos em oportunidades de crescimento e desenvolvimento sustentável.</p>
     </div>
   )
 }
